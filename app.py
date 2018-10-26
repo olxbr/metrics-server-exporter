8e19e7d565d9b5606207cefcedebe4eb9e84833f
Thiago Freitas <deadcow@archlinux.com.br>
2018-10-26 16:05:40 -0300

    feat(app) add blacklist support for pods,containers or namespaces

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ from prometheus_client.core import REGISTRY
 class MetricsServerExporter:
 
     def __init__(self):
-        self.svc_token     = os.environ.get('K8S_FILEPATH_TOKEN', '/var/run/secrets/kubernetes.io/serviceaccount/token')
-        self.api_url       = os.environ.get('K8S_ENDPOINT', 'https://kubernetes.default.svc')
-        self.api_nodes_url = "{}/apis/metrics.k8s.io/v1beta1/nodes".format(self.api_url)
-        self.api_pods_url  = "{}/apis/metrics.k8s.io/v1beta1/pods".format(self.api_url)
+        self.svc_token       = os.environ.get('K8S_FILEPATH_TOKEN', '/var/run/secrets/kubernetes.io/serviceaccount/token')
+        self.api_url         = os.environ.get('K8S_ENDPOINT', 'https://kubernetes.default.svc')
+        self.names_blacklist = os.environ.get('NAMES_BLACKLIST', '').split(',')
+        self.api_nodes_url   = "{}/apis/metrics.k8s.io/v1beta1/nodes".format(self.api_url)
+        self.api_pods_url    = "{}/apis/metrics.k8s.io/v1beta1/pods".format(self.api_url)
 
         self.token = self.set_token()
 
@@ -97,8 +98,9 @@ class MetricsServerExporter:
                 pod_container_mem = container['usage']['memory']
                 pod_container_mem = pod_container_mem.translate(str.maketrans('', '', string.ascii_letters))
 
-                metrics_pods_mem.add_sample('kube_metrics_server_pods_mem', value=int(pod_container_mem), labels={ 'pod_name': pod_name, 'pod_namespace': pod_namespace, 'pod_container_name': pod_container_name })
-                metrics_pods_cpu.add_sample('kube_metrics_server_pods_cpu', value=int(pod_container_cpu), labels={ 'pod_name': pod_name, 'pod_namespace': pod_namespace, 'pod_container_name': pod_container_name })
+                if not any(blacklisted in self.names_blacklist for blacklisted in [pod_container_name, pod_name, pod_namespace]):
+                    metrics_pods_mem.add_sample('kube_metrics_server_pods_mem', value=int(pod_container_mem), labels={ 'pod_name': pod_name, 'pod_namespace': pod_namespace, 'pod_container_name': pod_container_name })
+                    metrics_pods_cpu.add_sample('kube_metrics_server_pods_cpu', value=int(pod_container_cpu), labels={ 'pod_name': pod_name, 'pod_namespace': pod_namespace, 'pod_container_name': pod_container_name })
 
         yield metrics_pods_mem
         yield metrics_pods_cpu
